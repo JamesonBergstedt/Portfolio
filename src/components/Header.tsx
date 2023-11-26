bc43ca388bc65207ffb5c60649864096a7e4449c
Jameson Bergstedt <jabergstedt@gmail.com>
2023-11-26 16:15:32 -0500

    added css to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,20 @@ const Header = () => {
   return (
     <>
       <div className="flex px-[3%] h-[100vh] relative justify-center">
-        <div className="h-full border-yellow border-2 flex-col justify-center flex w-[50%]">
-          <div className="border-red border-2 min-h-[20vh]">
+        <div className="h-full border-yellow border-2 flex-col justify-center flex w-[50vw] gap-[5%]">
+          <div className="border-red border-2 min-h-[15vh]">
             <motion.span
               initial={{ opacity: 0, top: "15%" }}
               animate={{ opacity: 1, top: "30%" }}
               transition={{ duration: 2, ease: "easeOut" }}
-              className="bg-green rounded-2xl p-[5%] text-header_lg font-azeret font-semibold text-green_text whitespace-nowrap relative h-fit"
+              className="bg-green rounded-2xl p-[5vmin] text-header_lg font-azeret font-semibold text-green_text whitespace-nowrap relative h-fit"
             >
               Hi, my name is
               <motion.div
                 initial={{ opacity: 0, top: "-70%" }}
                 animate={{ opacity: 1, top: "10%" }}
                 transition={{ duration: 3, ease: "easeOut" }}
-                className="bg-lightgreen w-full h-full p-[10%] rounded-2xl absolute top-[10%] -z-50 left-[5%]"
+                className="bg-lightgreen w-full h-full p-[5vmin] rounded-2xl absolute top-[10%] -z-50 left-[5%]"
               ></motion.div>
             </motion.span>
           </div>
@@ -29,25 +29,25 @@ const Header = () => {
             Hi, my name is
             <div className="bg-lightgreen w-full h-full p-[10%] rounded-2xl absolute top-[10%] -z-50 left-[5%]"></div>
           </span> */}
-          <div className="border-blue border-2 min-h-[20vh]">
+          <div className="border-blue border-2 min-h-[15vh] ml-[5vw]">
             <motion.span
               initial={{ opacity: 0, top: "60%" }}
               animate={{ opacity: 1, top: "40%" }}
               transition={{ duration: 2, ease: "easeOut" }}
-              className="bg-yellow rounded-2xl p-[5%] text-header_lg font-azeret font-semibold text-yellow_text whitespace-nowrap relative h-fit"
+              className="bg-yellow rounded-2xl p-[5vmin] text-header_lg font-azeret font-semibold text-yellow_text whitespace-nowrap relative h-fit"
             >
               Jameson Bergstedt
               <motion.div
                 initial={{ opacity: 0, top: "80%" }}
                 animate={{ opacity: 1, top: "10%" }}
                 transition={{ duration: 3, ease: "easeOut" }}
-                className="bg-lightyellow w-full h-full p-[10%] rounded-2xl absolute top-[10%] -z-50 left-[5%]"
+                className="bg-lightyellow w-full h-full p-[5vmin] rounded-2xl absolute top-[10%] -z-50 left-[5%]"
               ></motion.div>
             </motion.span>
           </div>
         </div>
 
-        <div className=" flex items-center border-red border-2">
+        <div className=" flex items-center border-red border-2 mt-[15vh]">
           <div className="bg-[url('/jameson_picture.jpg')] h-[35vmin] w-[35vmin]  rounded-full bg-cover relative top-[-10%]">
             <div className=" bg-lightred rounded-full absolute h-[36vmin] w-[36vmin] bottom-[-5%] left-[3%] -z-50"></div>
           </div>
