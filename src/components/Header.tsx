a815236bcaa32bd4119cafc21d5f7be68b072604
Jameson Bergstedt <jabergstedt@gmail.com>
2023-11-27 11:59:37 -0500

    playing with animations

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,23 +4,41 @@ const Header = () => {
   return (
     <>
       <div className="flex px-[3%] h-[85vh] relative justify-center mb-[5%]">
-        <div className="h-full flex-col justify-center flex w-[50vw] gap-[5%]">
-          <div className=" min-h-[15vh]">
-            <motion.span
-              initial={{ opacity: 0, top: "15%" }}
-              animate={{ opacity: 1, top: "30%" }}
-              transition={{ duration: 2, ease: "easeOut" }}
-              className="bg-green rounded-2xl p-[5vmin] text-header_lg font-azeret font-semibold text-green_text whitespace-nowrap relative h-fit"
-            >
-              Hi, my name is
-              <motion.div
-                initial={{ opacity: 0, top: "-70%" }}
-                animate={{ opacity: 1, top: "10%" }}
-                transition={{ duration: 3, ease: "easeOut" }}
-                className="bg-lightgreen w-full h-full p-[5vmin] rounded-2xl absolute top-[10%] -z-50 left-[5%]"
-              ></motion.div>
-            </motion.span>
-          </div>
+        <div className="h-full flex-col justify-center flex w-[50vw] lg:gap-[5%]">
+          <motion.div
+            initial={{
+              zIndex: 100,
+              transform: "translateZ(8px) translateY(-2px)",
+            }}
+            animate={{
+              zIndex: 100,
+              transform: "translateZ(32px) translateY(-8px)",
+            }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "mirror",
+              duration: 2,
+              east: "easeInOut",
+            }}
+            className=" min-h-[15vh] flex"
+          >
+            <div className=" min-h-[15vh] ml-[5vw]">
+              <motion.span
+                initial={{ opacity: 0, top: "60%" }}
+                animate={{ opacity: 1, top: "40%" }}
+                transition={{ duration: 2, ease: "easeOut" }}
+                className="bg-green rounded-2xl p-[5vmin] text-header_lg font-azeret font-semibold text-green_text whitespace-nowrap relative h-fit"
+              >
+                Hi, my name is
+                <motion.div
+                  initial={{ opacity: 0, top: "80%" }}
+                  animate={{ opacity: 1, top: "10%" }}
+                  transition={{ duration: 3, ease: "easeOut" }}
+                  className="bg-lightgreen w-full h-full p-[5vmin] rounded-2xl absolute top-[10%] -z-50 left-[5%] "
+                ></motion.div>
+              </motion.span>
+            </div>
+          </motion.div>
           {/* <span className="bg-green rounded-2xl p-[5%] text-header_lg font-azeret font-semibold text-green_text whitespace-nowrap relative top-[25%] left-[10%]">
             Hi, my name is
             <div className="bg-lightgreen w-full h-full p-[10%] rounded-2xl absolute top-[10%] -z-50 left-[5%]"></div>
@@ -29,22 +47,40 @@ const Header = () => {
             Hi, my name is
             <div className="bg-lightgreen w-full h-full p-[10%] rounded-2xl absolute top-[10%] -z-50 left-[5%]"></div>
           </span> */}
-          <div className=" min-h-[15vh] ml-[5vw]">
-            <motion.span
-              initial={{ opacity: 0, top: "60%" }}
-              animate={{ opacity: 1, top: "40%" }}
-              transition={{ duration: 2, ease: "easeOut" }}
-              className="bg-yellow rounded-2xl p-[5vmin] text-header_lg font-azeret font-semibold text-yellow_text whitespace-nowrap relative h-fit"
-            >
-              Jameson Bergstedt
-              <motion.div
-                initial={{ opacity: 0, top: "80%" }}
-                animate={{ opacity: 1, top: "10%" }}
-                transition={{ duration: 3, ease: "easeOut" }}
-                className="bg-lightyellow w-full h-full p-[5vmin] rounded-2xl absolute top-[10%] -z-50 left-[5%]"
-              ></motion.div>
-            </motion.span>
-          </div>
+          <motion.div
+            initial={{
+              zIndex: 100,
+              transform: "translateZ(8px) translateY(-2px)",
+            }}
+            animate={{
+              zIndex: 100,
+              transform: "translateZ(32px) translateY(-8px)",
+            }}
+            transition={{
+              repeat: Infinity,
+              repeatType: "mirror",
+              duration: 2,
+              east: "easeInOut",
+            }}
+            className=" min-h-[15vh] flex"
+          >
+            <div className=" min-h-[15vh] ml-[5vw]">
+              <motion.span
+                initial={{ opacity: 0, top: "60%" }}
+                animate={{ opacity: 1, top: "40%" }}
+                transition={{ duration: 2, ease: "easeOut" }}
+                className="bg-yellow rounded-2xl p-[5vmin] text-header_lg font-azeret font-semibold text-yellow_text whitespace-nowrap relative h-fit lg:left-[10%]"
+              >
+                Jameson Bergstedt
+                <motion.div
+                  initial={{ opacity: 0, top: "80%" }}
+                  animate={{ opacity: 1, top: "10%" }}
+                  transition={{ duration: 3, ease: "easeOut" }}
+                  className="bg-lightyellow w-full h-full p-[5vmin] rounded-2xl absolute top-[10%] -z-50 left-[5%] "
+                ></motion.div>
+              </motion.span>
+            </div>
+          </motion.div>
         </div>
 
         <div className=" flex items-center mt-[15vh]">
